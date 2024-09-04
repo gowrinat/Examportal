ad26983458130e89f3e53dea6922d770825d8381
Gowrinath <98816153+gowrinat@users.noreply.github.com>
2024-09-04 10:04:35 +0530

    Add files via upload

---
+++ src/main/java/com/example/Exam_protal/ExamProtalApplication.java
@@ -0,0 +1,14 @@
+package com.example.Exam_protal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class ExamProtalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamProtalApplication.class, args);
+	}
+}
